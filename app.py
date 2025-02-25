ccfaac66211e168a91d5888fbefcd2dd6aceb4d8
Stephen <sshieldsmma@ip-172-31-15-214.us-east-2.compute.internal>
2025-02-25 19:04:24 +0000

    update

--- app.py
+++ app.py
@@ -165,4 +165,5 @@ def filter_tasks(filter):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    
